1cbf7fe122c0a89cec9f678aab8c6d68b3116d52
Kei Kurono <keikurono01@gmail.com>
2020-08-29 03:13:44 -0500

    adding class 11

---
+++ clase_11/https_server.c
@@ -0,0 +1,301 @@
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+const char *get_content_type(const char* path) {
+    const char *last_dot = strrchr(path, '.');
+    if (last_dot) {
+        if (strcmp(last_dot, ".css") == 0) return "text/css";
+        if (strcmp(last_dot, ".csv") == 0) return "text/csv";
+        if (strcmp(last_dot, ".gif") == 0) return "image/gif";
+        if (strcmp(last_dot, ".htm") == 0) return "text/html";
+        if (strcmp(last_dot, ".html") == 0) return "text/html";
+        if (strcmp(last_dot, ".ico") == 0) return "image/x-icon";
+        if (strcmp(last_dot, ".jpeg") == 0) return "image/jpeg";
+        if (strcmp(last_dot, ".jpg") == 0) return "image/jpeg";
+        if (strcmp(last_dot, ".js") == 0) return "application/javascript";
+        if (strcmp(last_dot, ".json") == 0) return "application/json";
+        if (strcmp(last_dot, ".png") == 0) return "image/png";
+        if (strcmp(last_dot, ".pdf") == 0) return "application/pdf";
+        if (strcmp(last_dot, ".svg") == 0) return "image/svg+xml";
+        if (strcmp(last_dot, ".txt") == 0) return "text/plain";
+    }
+
+    return "application/octet-stream";
+}
+
+
+SOCKET create_socket(const char* host, const char *port) {
+    printf("Configuring local address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo *bind_address;
+    getaddrinfo(host, port, &hints, &bind_address);
+
+    printf("Creating socket...\n");
+    SOCKET socket_listen;
+    socket_listen = socket(bind_address->ai_family,
+            bind_address->ai_socktype, bind_address->ai_protocol);
+    if (!ISVALIDSOCKET(socket_listen)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+
+    printf("Binding socket to local address...\n");
+    if (bind(socket_listen,
+                bind_address->ai_addr, bind_address->ai_addrlen)) {
+        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+    freeaddrinfo(bind_address);
+
+    printf("Listening...\n");
+    if (listen(socket_listen, 10) < 0) {
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+
+    return socket_listen;
+}
+
+
+
+#define MAX_REQUEST_SIZE 2047
+
+struct client_info {
+    socklen_t address_length;
+    struct sockaddr_storage address;
+    SOCKET socket;
+    SSL *ssl;
+    char request[MAX_REQUEST_SIZE + 1];
+    int received;
+    struct client_info *next;
+};
+
+static struct client_info *clients = 0;
+
+struct client_info *get_client(SOCKET s) {
+    struct client_info *ci = clients;
+
+    while(ci) {
+        if (ci->socket == s)
+            break;
+        ci = ci->next;
+    }
+
+    if (ci) return ci;
+    struct client_info *n =
+        (struct client_info*) calloc(1, sizeof(struct client_info));
+
+    if (!n) {
+        fprintf(stderr, "Out of memory.\n");
+        exit(1);
+    }
+
+    n->address_length = sizeof(n->address);
+    n->next = clients;
+    clients = n;
+    return n;
+}
+
+
+void drop_client(struct client_info *client) {
+    SSL_shutdown(client->ssl);
+    CLOSESOCKET(client->socket);
+    SSL_free(client->ssl);
+
+    struct client_info **p = &clients;
+
+    while(*p) {
+        if (*p == client) {
+            *p = client->next;
+            free(client);
+            return;
+        }
+        p = &(*p)->next;
+    }
+
+    exit(1);
+}
+
+
+const char *get_client_address(struct client_info *ci) {
+    static char address_buffer[100];
+    getnameinfo((struct sockaddr*)&ci->address,
+            ci->address_length,
+            address_buffer, sizeof(address_buffer), 0, 0,
+            NI_NUMERICHOST);
+    return address_buffer;
+}
+
+
+
+
+fd_set wait_on_clients(SOCKET server) {
+    fd_set reads;
+    FD_ZERO(&reads);
+    FD_SET(server, &reads);
+    SOCKET max_socket = server;
+
+    struct client_info *ci = clients;
+
+    while(ci) {
+        FD_SET(ci->socket, &reads);
+        if (ci->socket > max_socket)
+            max_socket = ci->socket;
+        ci = ci->next;
+    }
+
+    if (select(max_socket+1, &reads, 0, 0, 0) < 0) {
+        fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+
+    return reads;
+}
+
+
+void good_bye(struct client_info *client) {
+    const char *bye = "Good Bye";
+    SSL_write(client->ssl, bye, strlen(bye));
+    drop_client(client);
+}
+
+
+
+int main() {
+
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+
+    SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
+    if (!ctx) {
+        fprintf(stderr, "SSL_CTX_new() failed.\n");
+        return 1;
+    }
+
+
+    if (!SSL_CTX_use_certificate_file(ctx, "cert.pem" , SSL_FILETYPE_PEM)
+    || !SSL_CTX_use_PrivateKey_file(ctx, "key.pem", SSL_FILETYPE_PEM)) {
+        fprintf(stderr, "SSL_CTX_use_certificate_file() failed.\n");
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+    SOCKET server = create_socket(0, "8080");
+
+
+    while(1) {
+
+        fd_set reads;
+        reads = wait_on_clients(server);
+
+        if (FD_ISSET(server, &reads)) {
+            puts("cliente nuevo ...");
+            struct client_info *client = get_client(-1);
+
+            client->socket = accept(server,
+                    (struct sockaddr*) &(client->address),
+                    &(client->address_length));
+
+            if (!ISVALIDSOCKET(client->socket)) {
+                fprintf(stderr, "accept() failed. (%d)\n",
+                        GETSOCKETERRNO());
+                return 1;
+            }
+
+
+            client->ssl = SSL_new(ctx);
+            if (!client->ssl) {
+                fprintf(stderr, "SSL_new() failed.\n");
+                return 1;
+            }
+
+            SSL_set_fd(client->ssl, client->socket);
+            if (SSL_accept(client->ssl) != 1) {
+                //SSL_get_error(client->ssl, SSL_accept(...));
+                ERR_print_errors_fp(stderr);
+                drop_client(client);
+            } else {
+                printf("New connection from %s.\n",
+                        get_client_address(client));
+
+                printf ("SSL connection using %s\n",
+                        SSL_get_cipher(client->ssl));
+                        
+                SSL_write(client->ssl, "Hello Client!\n", 14);
+            }
+        }
+
+
+        struct client_info *client = clients;
+        while(client) {
+            struct client_info *next = client->next;
+
+            if (FD_ISSET(client->socket, &reads)) {
+                if (MAX_REQUEST_SIZE == client->received) {
+                    good_bye(client);
+                    client = next;
+                    continue;
+                }
+
+                int r = SSL_read(client->ssl,
+                        client->request,
+                        MAX_REQUEST_SIZE);
+
+                if (r < 1) {
+                    printf("Unexpected disconnect from %s.\n",
+                            get_client_address(client));
+                    drop_client(client);
+
+                } else {
+                    client->received += r;
+                    client->request[client->received] = 0;
+
+                    printf("text received: %s\n", client->request);
+                    char mresponse[MAX_REQUEST_SIZE + 50];
+                    sprintf(mresponse, "ECHO: %s\n", client->request);
+                    SSL_write(client->ssl, mresponse, strlen(mresponse));
+                }
+            }
+
+            client = next;
+        }
+
+    } //while(1)
+
+
+    printf("\nClosing socket...\n");
+    CLOSESOCKET(server);
+    SSL_CTX_free(ctx);
+
+    printf("Finished.\n");
+    return 0;
+}
+
